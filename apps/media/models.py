caadc70104edaefd241e2c5d1e1b6afc51193c3d
Aldair215 <73908755+Aldair215@users.noreply.github.com>
2025-09-11 03:02:02 -0300

    conf notebook 2v4

---
+++ apps/media/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+
+class Media(models.Model):
+    MEDIA_TYPES = (
+        ('image', 'Image'),
+        ('video', 'Video'),
+        ('document', 'Document'),
+        ('audio', 'Audio'),
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    order = models.PositiveIntegerField(default=0)
+    name = models.CharField(max_length=255)
+    size = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    key = models.CharField(max_length=255)
+    media_type = models.CharField(max_length=30, choices=MEDIA_TYPES)
